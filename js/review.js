7e155d144ec9105ba79a7f1e550dacea46a39188
GaryRam <126gramirez@frhsd.com>
2024-10-02 12:44:11 -0400

    first commit

---
+++ js/review.js
@@ -0,0 +1,31 @@
+console.log('This is external')
+console.log('work on your typing skillzzzz');
+
+//varibales
+let variableName;
+const myName = 'Gary Ramirez'
+
+let lukeage = 16;
+let tylerage = 17;
+let kevinage = 18;
+let mcdonaldage = 33;
+
+let old = lukeage + mcdonaldage;
+console.log('Old age = ' + old)
+console.log('old');
+console.log(old+kevinage);
+old+=kevinage
+console.log(old);
+
+//arithmatic
+console.log(10 % 2);
+console.log(14 % 2);
+console.log(34 % 2);
+
+for (let x = 0; x <=500; x++)
+{
+    if (x % 2 ==0) 
+    {
+        console.log(x);
+    }
+}
